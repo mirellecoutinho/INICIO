5d3b6b87fdeb171ed9789c6da3e905946a95ca14
Mirelle Coutinho <101526504+mirellecoutinho@users.noreply.github.com>
2022-03-13 18:06:52 -0300

    Add files via upload

---
+++ IMC.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+
+float peso = 74;
+float altura = 1.63;
+
+float IMC = peso/(altura*altura);
+  
+  printf("Seu IMC é: %.1f\n",IMC);
+
+
+  
+  return 0;
+}
